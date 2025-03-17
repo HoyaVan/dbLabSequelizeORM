74af9ddf1bd880b23558f3ecac57aca4bf4b9f07
Yuho Lim <dbgh7894@gmail.com>
2025-03-17 16:09:11 -0700

    updated db connection

--- databaseConnection.js
+++ databaseConnection.js
@@ -2,7 +2,6 @@ const mysql = require('mysql2/promise');
 require('dotenv').config();
 
 const dbConfigLocal = {
-	url: process.env.DB_URL,
 	host: process.env.DB_HOST,
 	user: process.env.DB_USER,
 	password: process.env.DB_PASSWORD,
