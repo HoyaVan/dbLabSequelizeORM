0087910320a37e52d3b841892b0a488bf5dd0dba
Yuho Lim <dbgh7894@gmail.com>
2025-03-17 18:08:47 -0700

    added DB_URL

--- databaseConnectionSequelize.js
+++ databaseConnectionSequelize.js
@@ -1,7 +1,6 @@
 require('dotenv').config();
 
-const dbConfigLocal =
-`mysql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}}/${process.env.DB_NAME}?ssl-mode=REQUIRED`;
+const dbConfigLocal = process.env.DB_URL;
 
 var databaseConnectionString = dbConfigLocal;
 module.exports = databaseConnectionString;
